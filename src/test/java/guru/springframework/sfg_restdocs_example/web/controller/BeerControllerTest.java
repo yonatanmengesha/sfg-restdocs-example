e33ea09210c0e0d6f385d7e8bbfaa5168accf427
yonatanmengesha <yonatan.mengesha2017@gmail.com>
2025-06-18 14:47:06 -0400

    formating the classes

--- src/test/java/guru/springframework/sfg_restdocs_example/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/sfg_restdocs_example/web/controller/BeerControllerTest.java
@@ -25,7 +25,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(BeerController.class)
 @ComponentScan(basePackages = "guru.springframework.sfg_restdocs_example.web.mappers")
-
 class BeerControllerTest {
 
 
